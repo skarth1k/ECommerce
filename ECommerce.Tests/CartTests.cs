963f6ae7b85791bda63904e9eefc1335716b8ec5
Karthik <karthik_in@rocketmail.com>
2020-09-15 22:11:33 +0530

    Add project files.

---
+++ ECommerce.Tests/CartTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ECommerce.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CartTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
