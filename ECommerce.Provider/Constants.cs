ee6b86e98d777aee7ffc91f75e51db93ccd23987
Karthik <karthik_in@rocketmail.com>
2020-09-16 09:25:01 +0530

    Product Helper class for getting the list of products by productNames or get Product by name.
    Created a constant to access the Dictionary keys to fetch in constant time

---
+++ ECommerce.Provider/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Provider
+{
+    public static class Constants
+    {
+        public const string A = "A";
+        public const string B = "B";
+        public const string C = "C";
+        public const string D = "D";
+        public const string E = "E";
+    }
+}
---
+++ ECommerce.Provider/ProductHelper.cs
@@ -0,0 +1,66 @@
+﻿using ECommerce.Contracts;
+using System.Collections.Generic;
+
+namespace ECommerce.Provider
+{
+    public class ProductHelper
+    {
+        private static readonly Dictionary<string, Product> products = new Dictionary<string, Product>
+        {
+            {
+                Constants.A,
+                new Product
+                {
+                    Id = 1,
+                    Name ="A",
+                    Price = 50
+                }
+            },
+            {
+                Constants.B,
+                new Product
+                {
+                    Id = 2,
+                    Name ="B",
+                    Price = 30
+                }
+            },
+            {
+                Constants.C,
+                new Product
+                {
+                    Id = 3,
+                    Name ="C",
+                    Price = 20
+                }
+            },
+            {
+                Constants.D,
+                new Product
+                {
+                    Id = 4,
+                    Name ="D",
+                    Price = 15
+                }
+            }
+        };
+
+
+        public static Product Get(string productName)
+        {
+            return products[productName];
+        }
+
+        public static List<Product> GetProducts(params string[] productNames)
+        {
+            var requestedProducts = new List<Product>();
+
+            foreach (var productName in productNames)
+            {
+                requestedProducts.Add(products[productName]);
+            }
+
+            return requestedProducts;
+        }
+    }
+}
