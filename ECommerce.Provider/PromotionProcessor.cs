b5825e37a14622ef6be042eb550fdd95768e36d0
Karthik <karthik_in@rocketmail.com>
2020-09-16 12:15:46 +0530

    Created the Promotion Processor class which takes list of promotion rules as a dependency.

---
+++ ECommerce.Provider/PromotionProcessor.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Contracts;
+using System.Collections.Generic;
+
+namespace ECommerce.Provider
+{
+    public class PromotionProcessor
+    {
+        private List<IPromotionRule> promotionRules;
+        public PromotionProcessor(List<IPromotionRule> promotionRules)
+        {
+            this.promotionRules = promotionRules;
+        }
+    }
+}
