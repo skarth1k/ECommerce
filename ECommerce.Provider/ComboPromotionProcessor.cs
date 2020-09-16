260dcae2f6ecf798c4182b8f8163089130566587
Karthik <karthik_in@rocketmail.com>
2020-09-16 11:55:55 +0530

    Testcases for creating rules which will be useful for creating Promotions in the  future

---
+++ ECommerce.Provider/ComboPromotionProcessor.cs
@@ -0,0 +1,69 @@
+﻿using ECommerce.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace ECommerce.Provider
+{
+    public class ComboPromotionProcessor : IPromotionRule, IPromotion
+    {
+        List<ComboPromotion> comboPromotions = new List<ComboPromotion>
+        {
+            new ComboPromotion
+            {
+                Price = 30,
+                Quantity = 1,
+                Products = ProductHelper.GetProducts(Constants.C, Constants.D),
+                Type = PromotionType.Combo
+            }
+        };
+
+        public double TotalPromotions => comboPromotions.Count;
+
+        public void Add(Promotion promotion)
+        {
+            if (promotion.Type != PromotionType.Combo)
+                throw new InvalidOperationException("Invalid Promotion");
+
+            var comboPromo = promotion as ComboPromotion;
+
+            if (comboPromo != null)
+            {
+                bool isValidPromotion = GetIsValidPromotion(comboPromo);
+
+                if (isValidPromotion)
+                    comboPromotions.Add(comboPromo);
+            }
+        }
+
+        public void Remove(Promotion promotion)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool GetIsValidPromotion(ComboPromotion promotion)
+        {
+            var result = comboPromotions
+                .FirstOrDefault(i => new ComboComparer().Compare(i, promotion) == 0);
+
+            return result == null;
+        }
+
+
+
+        class ComboComparer : IComparer<ComboPromotion>
+        {
+            public int Compare([AllowNull] ComboPromotion x, [AllowNull] ComboPromotion y)
+            {
+                if (x.Products.Count != y.Products.Count)
+                    return -1;
+
+                if (x.Products.Count == y.Products.Count && x.Quantity == y.Quantity)
+                    return 0;
+
+                return 1;
+            }
+        }
+    }
+}
--- ECommerce.Tests/CartTests.cs
+++ ECommerce.Tests/CartTests.cs
@@ -1,6 +1,7 @@
 using ECommerce.Contracts;
 using ECommerce.Provider;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace ECommerce.Tests
@@ -55,15 +56,59 @@ namespace ECommerce.Tests
         public void CreateIndividualPromotionTest()
         {
             IPromotion createPromotion = new IndividualPromotionProcessor();
-            createPromotion.Add(new IndividualPromotion 
-            { 
-                Price = 180, 
-                Quantity = 5, 
-                Type = PromotionType.Individual, 
-                Product = ProductHelper.Get(Constants.A) 
+            createPromotion.Add(new IndividualPromotion
+            {
+                Price = 180,
+                Quantity = 5,
+                Type = PromotionType.Individual,
+                Product = ProductHelper.Get(Constants.A)
             });
 
             Assert.AreEqual(3, createPromotion.TotalPromotions);
+        }
+
+        [Test]
+        public void CreateIndividualPromotion_InvalidTest()
+        {
+            IPromotion createPromotion = new IndividualPromotionProcessor();           
+
+            Assert.Throws<InvalidOperationException>(() => createPromotion.Add(new IndividualPromotion
+            {
+                Price = 180,
+                Quantity = 5,
+                Type = PromotionType.Combo,
+                Product = ProductHelper.Get(Constants.A)
+            }));
+        }
+
+
+        [Test]
+        public void CreateComboPromotionTest()
+        {
+            IPromotion createPromotion = new ComboPromotionProcessor();
+            createPromotion.Add(new ComboPromotion
+            {
+                Price = 80,
+                Quantity = 2,
+                Type = PromotionType.Combo,
+                Products = ProductHelper.GetProducts(Constants.A, Constants.B, Constants.C)
+            });
+
+            Assert.AreEqual(2, createPromotion.TotalPromotions);
+        }
+
+        [Test]
+        public void CreateComboPromotion_InvalidTest()
+        {
+            IPromotion createPromotion = new ComboPromotionProcessor();           
+
+            Assert.Throws<InvalidOperationException>(() => createPromotion.Add(new ComboPromotion
+            {
+                Price = 80,
+                Quantity = 2,
+                Type = PromotionType.Individual,
+                Products = ProductHelper.GetProducts(Constants.A, Constants.B, Constants.C)
+            }));
         }
     }
 }
